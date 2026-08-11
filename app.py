d913465ff45edb82820e985ed4b32cfcb990c0b3
kush10521 <kushsingh10521@gmail.com>
2026-08-12 02:00:56 +0530

    add division code

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ def sub(a, b):
 def mul(a, b):
     return a * b
 
+def div(a, b):
+    return a / b
+
 
 if __name__ == "__main__":
     result1 = add(4, 5)
     result2 = sub(4, 1)
     result3 = mul(4, 5)
+    result4 = div(4, 2)
 
-    print(result1, result2, result3)
+    print(result1, result2, result3, result4)
