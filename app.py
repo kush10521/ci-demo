15c2db6cee7899588db18027de9528c3c0b35466
kush10521 <kushsingh10521@gmail.com>
2026-08-12 01:27:52 +0530

    some more minor bug fix

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ def add(a, b):
 
 
 def sub(a, b):
-    return a-b
+    return a - b
 
 
 def mul(a, b):
-    return a*b
+    return a * b
 
 
 if __name__ == "__main__":
     result1 = add(4, 5)
     result2 = sub(4, 1)
+    result3 = mul(4, 5)
 
-    print(result1, result2)
+    print(result1, result2, result3)
     
